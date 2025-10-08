1efde3ff7166a8493e0deea2836baf73e3246b86
XxdrixX <daveranola351@gmail.com>
2025-10-08 19:03:52 +0100

    spring initalizr

---
+++ src/test/java/com/example/spacex_backend/SpacexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
