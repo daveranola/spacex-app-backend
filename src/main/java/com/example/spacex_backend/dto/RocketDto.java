3fa8f0e66a0506b9329df3a8c7d6dda02b4904d1
XxdrixX <daveranola351@gmail.com>
2025-10-11 15:28:10 +0100

    dto modified to hold rocket info

--- src/main/java/com/example/spacex_backend/dto/RocketDto.java
+++ src/main/java/com/example/spacex_backend/dto/RocketDto.java
@@ -1,14 +1,61 @@
 package com.example.spacex_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.List;
 
-public record RocketDto(String id, String name, Height height,
-                        Diameter diameter, Mass mass,
-                        List<String> flickr_images,
-                        boolean active,
-                        String description,
-                        long cost_per_launch) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record RocketDto(
+        String id,
+        String name,
+        String type,
+        boolean active,
+        int stages,
+        int boosters,
+        Height height,
+        Diameter diameter,
+        Mass mass,
+        Engines engines,
+        FirstStage firstStage,
+        SecondStage secondStage,
+        List<String> flickrImages,
+        String description,
+        String company,
+        String country,
+        String wikipedia,
+        String firstFlight,
+        long costPerLaunch,
+        int successRatePct
+) {
     public record Height(double meters, double feet) {}
     public record Diameter(double meters, double feet) {}
     public record Mass(double kg, double lb) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Engines(
+            Isp isp,
+            int number,
+            String type,
+            String version,
+            String layout,
+            Integer engineLossMax,
+            String propellant1,
+            String propellant2,
+            double thrustToWeight
+    ) {
+        public record Isp(Integer seaLevel, Integer vacuum) {}
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record FirstStage(
+            Integer engines
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record SecondStage(
+            Integer engines
+    ) {}
 }
